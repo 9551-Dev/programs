143cd4fe39677aa50592fe89490cc19687a9e1c5
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-03-21 21:02:38 +0100

    Update AE2display.lua

--- AE2display.lua
+++ AE2display.lua
@@ -1,28 +1,47 @@
------------CONFIG-------------
-local cfg = {
-    update_time = 0,
-    log_frequency = 1
-}
-------------------------------
- 
 if not fs.exists("log.lua") then
     shell.run("wget https://github.com/9551-Dev/apis/raw/main/log.lua")
 end
 local history = {}
-if fs.exists("ae.history") then
+local cache = {}
+if not fs.exists("ae.cfg") then
+    local file = fs.open("ae.cfg","w")
+    file.write(textutils.serialise({
+        update_time=0,
+        log_frequency=1,
+        use_display_names=true,
+        keep_log_history=true
+    }))
+    file.close()
+end
+
+local file = fs.open("ae.cfg","r")
+local cfg = textutils.unserialise(file.readAll())
+file.close()
+
+if fs.exists("ae.history") and cfg.keep_log_history then
     local file = fs.open("ae.history","r")
     local data = file.readAll()
     file.close()
     history = textutils.unserialise(data)
+end
+if fs.exists("ae_name.cache") then
+    local file = fs.open("ae_name.cache","r")
+    local data = file.readAll()
+    file.close()
+    cache = textutils.unserialise(data)
 end
 local wrap = peripheral.find("appliedenergistics2:interface")
 if not wrap then error("failed ot find AE2 interface",0) end 
 local function convert_to_name(list)
     local out = {}
     for k,v in pairs(list) do
-        out[v.name.." dmg:"..tostring(v.damage or 0)] = v
+        out[v.name.."*"..tostring(v.damage or 0)] = v
     end
     return out
+end
+local function separate_name_damage(str)
+    local sout,n = str:match("(.-)%*(.+)")
+    return sout,tonumber(n)
 end
 local old_list = convert_to_name(wrap.listAvailableItems())
 local api = require("log")
@@ -42,7 +61,7 @@ end
 for k,v in pairs(history) do
     for k,_log in pairs(logs) do
         _log(":"..v.str,v.type)
-        table.remove(_log.history,#log.history)
+        table.remove(_log.history,#_log.history)
     end
 end
 local lt = debug.getmetatable(api.create_log(term)).__index
@@ -58,16 +77,59 @@ local function dump(i)
     file.write(textutils.serialise(lg.history))
     file.close()
 end
+local function get_item_display_name(name,damage)
+    local name_matches = wrap.findItems(name)
+    local threads = {}
+    local damage_match
+    for k,v in pairs(name_matches) do
+        table.insert(threads,function()
+            local meta = v.getMetadata()
+            if meta.damage == damage then
+                damage_match = meta.displayName
+            end
+        end)
+    end
+    parallel.waitForAll(unpack(threads))
+    return damage_match
+end
 for i=1,math.huge do
     local list = wrap.listAvailableItems()
     local list = convert_to_name(list)
     for k,v in pairs(old_list) do
+        local name = k
+        if cfg.use_display_names then
+            name = cache[k]
+            if not name then
+                name = get_item_display_name(separate_name_damage(k))
+                cache[k] = name
+                local file = fs.open("ae_name.cache","w")
+                file.write(textutils.serialise(cache))
+                file.close()
+                log("found unnamed item "..k.." adding into name cache....",lt.warn)
+            end
+        end
         if list[k] then
-            if v.count < list[k].count then log(k.." increased by: "..tostring(list[k].count-v.count),lt.update) end
-            if v.count > list[k].count then log(k.." dropped by: "..tostring(v.count-list[k].count),lt.error) end
-        else log("Removed "..k.." from system",lt.fatal) end
+            if v.count < list[k].count then log(name.." increased by: "..tostring(list[k].count-v.count),lt.update) end
+            if v.count > list[k].count then log(name.." dropped by: "..tostring(v.count-list[k].count),lt.error) end
+        else
+            log("Removed "..name.." from system",lt.fatal)
+        end
     end
-    for k,v in pairs(list) do if not old_list[k] then log("Added "..k.." into system",lt.success) end end
+    for k,v in pairs(list) do
+        local name = k
+        if cfg.use_display_names then
+            name = cache[k]
+            if not name then
+                name = get_item_display_name(separate_name_damage(k))
+                cache[k] = name
+                local file = fs.open("ae_name.cache","w")
+                file.write(textutils.serialise(cache))
+                file.close()
+                log("found unnamed item "..k.." adding into name cache....",lt.warn)
+            end
+        end
+        if not old_list[k] then log("Added "..name.." into system",lt.success) end
+    end
     old_list = list
     if i%cfg.log_frequency == 0 then dump(math.ceil(i/cfg.log_frequency)) end
     sleep(cfg.update_time)
