6f6b2db016d3450d412067be29a58925250e615d
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-04-02 16:02:15 +0200

    Update window.lua

--- window.lua
+++ window.lua
@@ -10,12 +10,30 @@ _G.args = args
 w,h = (w ~= "") and w or "20",(h ~= "") and h or "10"
 local api = require("GuiH.main")
 local gui = api.create_gui(term.current())
-
 local frame = gui.create.frame({
     x=2,2,width=tonumber(w),height=tonumber(h),
     dragger={
         x=1,y=1,width=tonumber(w),height=1
-    }
+    },
+    on_move=function(object,pos)
+        local term = object.canvas.term_object
+        local w,h = term.getSize()
+        object.window.reposition(
+            math.max(
+                math.min(
+                    pos.x+object.positioning.width,
+                    w+1
+                )-object.positioning.width,1
+            ),
+            math.max(
+                math.min(
+                    pos.y+object.positioning.height,
+                    h+1
+                )-object.positioning.height,1
+            )
+        )
+        return true
+    end
 })
 local shown = true
 local win = frame.window
@@ -37,10 +55,31 @@ local function update_thread()
         gui.update()
     end
 end
-local function update_shell()
+local shell_coro = coroutine.create(function()
     shell.run((program ~= "") and program or "sh",unpack(args))
+end)
+
+local function update_shell()
+    coroutine.resume(shell_coro)
+    while coroutine.status(shell_coro) ~= "dead" do
+        local ev_data = table.pack(os.pullEvent())
+        local ev = api.convert_event(table.unpack(ev_data,1,ev_data.n))
+        if api.valid_events[ev.name] then
+            local x,y = win.getPosition()
+            ev_data[3] = ev.x-x
+            ev_data[4] = ev.y-y
+            coroutine.resume(shell_coro,table.unpack(ev_data,1,ev_data.n))
+        else
+            coroutine.resume(shell_coro,table.unpack(ev_data,1,ev_data.n))
+        end
+    end
 end
-parallel.waitForAll(
-    update_thread,
-    update_shell
-)
+pcall(function()
+    parallel.waitForAll(
+        update_thread,
+        update_shell
+    )
+end)
+term.clear()
+term.setCursorPos(1,1)
+print("Ended window session") 
