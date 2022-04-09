6e1939b004b84db5ff47caa67bb7ffd9de7696f3
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-04-09 19:08:24 +0200

    Update window.lua

--- window.lua
+++ window.lua
@@ -7,7 +7,6 @@ local args = {...}
 table.remove(args,1)
 table.remove(args,1)
 table.remove(args,1)
-_G.args = args
 w,h = (w ~= "") and w or "20",(h ~= "") and h or "10"
 local api = require("GuiH.main")
 local gui = api.create_gui(g_win)
@@ -34,37 +33,32 @@ local frame = gui.create.frame({
             )
         )
         return true
-    end
+    end,
+    btn={true}
 })
 local shown = true
 local win = frame.window
 local ele = frame.child
 local w,h = win.getSize()
 local t_win = window.create(win,2,2,w-2,h-2)
+
 win.setCursorPos(1,1)
-win.setBackgroundColor(colors.gray)
-win.clear()
-win.setBackgroundColor(colors.lightGray)
-win.setCursorPos(1,1)
-win.write((" "):rep(w))
-win.setBackgroundColor(colors.black)
-t_win.clear()
-term.redirect(t_win)
-local function update_thread()
-    gui.update(0)
-    while true do
-        g_win.setVisible(false)
-        g_win.clear()
-        gui.update()
-        g_win.setVisible(true)
-    end
+win.blit((" "):rep(w),("f"):rep(w),("7"):rep(w))
+for i=2,h-1 do
+    win.setCursorPos(1,i)
+    win.blit(("\149"):rep(w),"0"..("f"):rep(w-1),"f"..("0"):rep(w-1))
 end
+win.setCursorPos(1,h)
+win.blit(("\131"):rep(w),("0"):rep(w),("f"):rep(w))
+
+
+t_win.clear()
+local old_term = term.redirect(t_win)
 local shell_coro = coroutine.create(function()
     shell.run((program ~= "") and program or "sh",unpack(args))
 end)
-
 local function update_shell()
-    coroutine.resume(shell_coro)
+    sleep(0.05)
     while coroutine.status(shell_coro) ~= "dead" do
         g_win.setVisible(false)
         g_win.clear()
@@ -75,17 +69,21 @@ local function update_shell()
             ev_data[3] = ev.x-x
             ev_data[4] = ev.y-y
         end
-        coroutine.resume(shell_coro,table.unpack(ev_data,1,ev_data.n))
+        if not (frame.dragged and ev_data[1] == "mouse_drag") and ev_data[1] ~= "key_up" then
+            coroutine.resume(shell_coro,table.unpack(ev_data,1,ev_data.n))
+        end
+        local cx,cy = win.getCursorPos()
+        local prg = shell.getRunningProgram():match("[^%/-]+$")
+        win.setCursorPos(1,1)
+        win.blit(prg..(" "):rep(w-#prg),("0"):rep(w),("7"):rep(w))
+        win.setCursorPos(cx,cy)
         win.redraw()
         g_win.setVisible(true)
     end
 end
-pcall(function()
-    parallel.waitForAny(
-        update_thread,
-        update_shell
-    )
-end)
+local ok,err = pcall(gui.execute,update_shell)
+term.redirect(old_term)
 term.clear()
 term.setCursorPos(1,1)
-print("Ended window session") 
+print("Ended window session. "..((ok == false) and err or ""))
+term.setCursorPos(1,2)
