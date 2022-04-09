571e68c5757361608a5f754cb404a9a9690350fa
9551 <74534592+9551-Dev@users.noreply.github.com>
2022-04-09 21:34:37 +0200

    Update window.lua

--- window.lua
+++ window.lua
@@ -47,10 +47,10 @@ win.setCursorPos(1,1)
 win.blit((" "):rep(w),("f"):rep(w),("7"):rep(w))
 for i=2,h-1 do
     win.setCursorPos(1,i)
-    win.blit(("\149"):rep(w),"0"..("f"):rep(w-1),"f"..("0"):rep(w-1))
+    win.blit(("\149"):rep(w),"8"..("f"):rep(w-1),"f"..("8"):rep(w-1))
 end
 win.setCursorPos(1,h)
-win.blit(("\131"):rep(w),("0"):rep(w),("f"):rep(w))
+win.blit("\141"..("\140"):rep(w-2).."\142",("8"):rep(w),("f"):rep(w))
 
 
 t_win.clear()
@@ -76,7 +76,7 @@ local function update_shell()
         local cx,cy = win.getCursorPos()
         local prg = shell.getRunningProgram():match("[^%/-]+$")
         win.setCursorPos(1,1)
-        win.blit(prg..(" "):rep(w-#prg),("0"):rep(w),("7"):rep(w))
+        win.blit(" "..prg..(" "):rep(w-#prg-1),("0"):rep(w),("7"):rep(w))
         win.setCursorPos(cx,cy)
         win.redraw()
         g_win.setVisible(true)
